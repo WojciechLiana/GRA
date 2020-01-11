9edea9d958410ca24cee31d5f796ad5f1a55e184
Wojciech Liana <wojtek.liana@gmail.com>
2020-01-11 13:35:46 +0100

    board & move

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    function main() {
+        const board = get_board();
+        const boardCnt = document.querySelector(".board_cnt");
+        console.log(boardCnt);
+        const Nairobi = new newPlayer("Nairobi", 3, 3);
+        Nairobi.createPlayer(board);
+        boardCnt.addEventListener('click', function () {
+            Nairobi.movePlayer(board);
+        });
+    }
+
+    main();
+
+    function get_board() {
+        const board = document.querySelectorAll(".board");
+        let boardTable = new Array(20);
+        for (let i = 0; i < boardTable.length; i++) {
+            boardTable[i] = new Array(20);
+        }
+        let counter = 0;
+        for (let i = 0; i < 20; i++) {
+            for (let j = 0; j < 20; j++) {
+                boardTable[i][j] = board[counter];
+                counter++;
+            }
+        }
+        return boardTable;
+    }
+
+    function newPlayer(name, posX, posY) {
+        this.name = name;
+        this.posX = posX;
+        this.posY = posY;
+        this.createPlayer = function (board) {
+            board[this.posX][this.posY].appendChild(createPlayer());
+        };
+        this.movePlayer = function (board) {
+            board[this.posX][this.posY].removeChild(board[this.posX][this.posY].firstChild);
+        };
+    }
+
+    function createPlayer() {
+        const player = document.createElement("div");
+        player.classList.add("player");
+        player.innerText = "P";
+        return player;
+    }
+
+});
