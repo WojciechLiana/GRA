4a1703d19022d50b1f3a59fe1ed9a063316b8b7b
Wojciech Liana <wojtek.liana@gmail.com>
2020-01-11 14:44:09 +0100

    movement with arrows

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     function main() {
-        const board = get_board();
-        const boardCnt = document.querySelector(".board_cnt");
-        console.log(boardCnt);
-        const Nairobi = new newPlayer("Nairobi", 1, 3);
-        Nairobi.createPlayer(board);
-        boardCnt.addEventListener('click', function () {
-            Nairobi.movePlayer(board);
+
+        const Nairobi = new New_player('Nairobi', 1, 3);
+        Nairobi.createPlayer(get_board()[0]);
+        document.addEventListener('keyup', function(e){
+            Nairobi.movePlayer(get_board()[0], e);
         });
+
+
     }
 
     main();
 
     function get_board() {
-        const board = document.querySelectorAll(".board");
+        const boardCnt = document.querySelector('.board_cnt');
+        const board = document.querySelectorAll('.board');
         let boardTable = new Array(20);
         for (let i = 0; i < boardTable.length; i++) {
             boardTable[i] = new Array(20);
@@ -26,25 +27,48 @@ document.addEventListener('DOMContentLoaded', function () {
                 counter++;
             }
         }
-        return boardTable;
+        return [boardTable, boardCnt];
     }
 
-    function newPlayer(name, posX, posY) {
+    function New_player(name, posX, posY) {
         this.name = name;
         this.posX = posX;
         this.posY = posY;
         this.createPlayer = function (board) {
-            board[this.posY][this.posX].appendChild(createPlayer());
+            board[this.posY][this.posX].appendChild(create_player());
         };
-        this.movePlayer = function (board) {
+        this.removePlayer = function(board){
             board[this.posY][this.posX].removeChild(board[this.posY][this.posX].firstChild);
+        }
+
+        this.movePlayer = function (board, key) {
+            if(key.keyCode === 37){
+                this.removePlayer(board);
+                this.posX -=1;
+                this.createPlayer(board);
+            }
+            if(key.keyCode === 38){
+                this.removePlayer(board);
+                this.posY -=1;
+                this.createPlayer(board);
+            }
+            if(key.keyCode === 39){
+                this.removePlayer(board);
+                this.posX +=1;
+                this.createPlayer(board);
+            }
+            if(key.keyCode === 40){
+                this.removePlayer(board);
+                this.posY +=1;
+                this.createPlayer(board);
+            }
         };
     }
 
-    function createPlayer() {
-        const player = document.createElement("div");
-        player.classList.add("player");
-        player.innerText = "P";
+    function create_player() {
+        const player = document.createElement('div');
+        player.classList.add('player');
+        player.innerText = 'P';
         return player;
     }
 
